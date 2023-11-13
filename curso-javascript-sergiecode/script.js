ebb79975f9c0c77533022418900b6e690f2bc9f4
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-13 11:51:35 +0200

    Operadores en JavaScript

--- curso-javascript-sergiecode/script.js
+++ curso-javascript-sergiecode/script.js
@@ -1,8 +1,17 @@
-// NUMEROS
-let variableNumero = 10 // Valor numérico
-let variableDecimal = 10.1 // Valor flotante
+// OPERADORES
+const variable = 'Valor de la variable' // Operador de asignación
 
-// TEXTO
-let variableTexto = 'Texto' // Valor String(cadena de caracteres)
-let variableTexto2 = 'Texto' // Valor String(cadena de caracteres)
-let variableTexto3 = `Esta es una variable distintas ${variableNumero}`
+const suma = 1 + 3 // + es operador aritmético de la suma
+const resta = 1 - 3 // - es operador aritmético de la resta
+const multiplicacion = 1 * 3 // * es operador aritmético de la multiplicación
+const division = 1 / 3 // / es operador aritmético de la división
+const resto = 5 % 2 // % es el operador aritmético que nos dará el resto de una división
+const exponente = 3 ** 3 // ** es el operador de la exponenciación
+
+let x = 5
+let y = 5
+
+x++ // aumentador
+y-- // disminuidor
+
+console.log(y)
---
+++ curso-javascript-sergiecode/script__01.js
@@ -0,0 +1,8 @@
+// NUMEROS
+let variableNumero = 10 // Valor numérico
+let variableDecimal = 10.1 // Valor flotante
+
+// TEXTO
+let variableTexto = 'Texto' // Valor String(cadena de caracteres)
+let variableTexto2 = 'Texto' // Valor String(cadena de caracteres)
+let variableTexto3 = `Esta es una variable distintas ${variableNumero}`
