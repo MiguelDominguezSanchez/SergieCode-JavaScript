df9cb12680c2810ad81d88869dcfec307eeaa43d
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-13 12:27:13 +0200

    Sintaxis de las variables

--- curso-javascript-sergiecode/script.js
+++ curso-javascript-sergiecode/script.js
@@ -1,17 +1,8 @@
-// OPERADORES
-const variable = 'Valor de la variable' // Operador de asignación
-
-const suma = 1 + 3 // + es operador aritmético de la suma
-const resta = 1 - 3 // - es operador aritmético de la resta
-const multiplicacion = 1 * 3 // * es operador aritmético de la multiplicación
-const division = 1 / 3 // / es operador aritmético de la división
-const resto = 5 % 2 // % es el operador aritmético que nos dará el resto de una división
-const exponente = 3 ** 3 // ** es el operador de la exponenciación
-
 let x = 5
-let y = 5
 
-x++ // aumentador
-y-- // disminuidor
+{
+	let y = 3
+	x -= y
 
-console.log(y)
+	console.log(x)
+}
---
+++ curso-javascript-sergiecode/script__03.js
@@ -0,0 +1,32 @@
+const $Variable = 'Nombre'
+const variable = 'Nombre'
+const Variable = 'Nombre'
+const _variable = 'Nombre'
+
+const variable1213 = 'Nombre'
+
+/*
+  Reglas:
+  1) Se puede iniciar con $, minúscula, mayúscula y _ (guión bajo)
+  2) No se puede iniciar con números pero si usuarios en el resto del nombre
+  3) No se puede usar caracteres especiales salvo $ y _
+  Información Extra:
+    Convenciones de nomenclatura
+  - Camel Case: Debes empezar con la primera letra minúscula y la primera letra de cada nueva palabra subsecuente en mayúscula: 
+  camelCase, variableNueva, newUser
+  - Snake Case: En Snake case, conocido también como "underscore case", se utiliza guión bajo en lugar de espacio para separar las palabras:
+  snake_case, variable_nueva, new_user
+  - Pascal Case: También conocido como "upper camel case" o "capital case", combina palabras con la primera letra en mayúscula: 
+  PascalCase, VariableNueva, NewUser
+  - Kebab Case:
+  En Kebab case se utiliza el guión para combinar las palabras (no se puede usar en variables pero si en keys de objetos): 
+  camel-case, variable-nueva, new-user
+
+*/
+
+let nombreApellido
+let nombreapellido
+
+nombreApellido = 'Pedro'
+
+console.log(nombreapellido)
