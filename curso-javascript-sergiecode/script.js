ce3d184ea06f4378f195dabcea8b71fa5fb06662
Miguel Dominguez <miguel.dominguez.developer@gmail.com>
2023-11-14 10:25:58 +0200

    Objetos literales

--- curso-javascript-sergiecode/script.js
+++ curso-javascript-sergiecode/script.js
@@ -1,21 +1,43 @@
-let num = '10'
-
-// Función clásica
-// function sumarDecena(numero) {
-// 	return numero + 12
+// let auto1 = {
+// 	marca: 'FIAT',
+// 	modelo: 'UNO',
+// 	peso: 1000,
+// 	color: 'Rojo',
 // }
 
-// Arrow function
-// let sumarDecena = (numero) => {
-// 	return numero + 12
+// let auto2 = {
+// 	marca: 'RENAULT',
+// 	modelo: '12',
+// 	peso: 1100,
+// 	color: 'Marrón',
 // }
-// Oneline Arrow function
-// let sumarDecena = (numero) => numero + 12
 
-// const respuesta = sumarDecena(parseInt(num))
+// console.log('Auto 1: ', auto1)
+// console.log('Auto 2: ', auto2)
 
-// console.log(respuesta)
+// console.log('El color del auto1 es: ', auto2.color)
 
-let funcion = () => console.log('Soy una función')
+// auto1.marca = 'BMW'
+// auto1.modelo = 'x5'
+// console.log(auto1)
 
-funcion()
+// console.log(auto1.marca)
+// console.log(auto1['marca'])
+
+const vendedor = {
+	nombre: 'Pedro',
+	apellido: 'González',
+	empresa: 'Auto S.A',
+	habilidadesBlandas: ['Carisma', 'Puntualidad'],
+	vender: function () {
+		return 'Pedro ha vendido un auto'
+	},
+	nombreCompleto: function () {
+		return this.nombre + ' ' + this.apellido
+	},
+}
+
+// console.log(vendedor.vender())
+// console.log(vendedor.nombreCompleto())
+// console.log(vendedor.nombre)
+console.log(vendedor.habilidadesBlandas)
---
+++ curso-javascript-sergiecode/script__09.js
@@ -0,0 +1,21 @@
+let num = '10'
+
+// Función clásica
+// function sumarDecena(numero) {
+// 	return numero + 12
+// }
+
+// Arrow function
+// let sumarDecena = (numero) => {
+// 	return numero + 12
+// }
+// Oneline Arrow function
+// let sumarDecena = (numero) => numero + 12
+
+// const respuesta = sumarDecena(parseInt(num))
+
+// console.log(respuesta)
+
+let funcion = () => console.log('Soy una función')
+
+funcion()
